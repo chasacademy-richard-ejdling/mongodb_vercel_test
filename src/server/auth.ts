82968bcbb8b36a647a6fc67fdc5e74edafd65b57
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-10-16 12:40:38 +0200

    reverted

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,10 +48,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    /* DiscordProvider({
+    DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
-    }), */
+    }),
     /**
      * ...add more providers here.
      *
